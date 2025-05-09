40565880e4d0ffe9cc3362b875674f7ccd9490f9
Arthur <lotuslovesurmom@gmail.com>
2025-05-09 14:54:48 +0200

    Les1

---
+++ Assets/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,38 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour
+{
+    private int Score;
+    private List<int> Coins = new List<int>();
+    
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Debug.Log("Game Start" + (i + 1));
+        }
+    }
+
+
+    void Update()
+    {
+        if (Score >= 50) 
+        {
+            Debug.Log("winnner");
+        }
+
+        if (Input.GetKeyUp(KeyCode.Escape)) 
+        {   int Coinvaulue = Random.Range(10, 12);
+            AddCoin(Coinvaulue);
+        }
+    }
+
+    void AddCoin(int coinValue)
+    {
+        Coins.Add(coinValue);
+        Score += coinValue;
+        Debug.Log("you got a coin" + Score);    
+    }
+}
