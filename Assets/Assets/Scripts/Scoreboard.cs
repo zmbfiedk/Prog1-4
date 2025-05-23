1ed142e7ba0fdb011895622db25eecf1bfc92983
Arthur <lotuslovesurmom@gmail.com>
2025-05-23 10:34:40 +0200

    les7 fix

--- Assets/Assets/Scripts/Scoreboard.cs
+++ Assets/Assets/Scripts/Scoreboard.cs
@@ -5,14 +5,26 @@ public class Scoreboard : MonoBehaviour
 {
     [SerializeField] private Scoremanager SC;
     private TMP_Text textfield;
+    private int previousScore;
 
     void Start()
     {
         textfield = GetComponent<TMP_Text>();
+        previousScore = SC.Score;
+        UpdateScoreDisplay();
     }
 
     void Update()
     {
+        if (SC.Score != previousScore)
+        {
+            UpdateScoreDisplay();
+        }
+    }
+
+    private void UpdateScoreDisplay()
+    {
+        previousScore = SC.Score;
         textfield.text = SC.Score.ToString();
     }
 }
